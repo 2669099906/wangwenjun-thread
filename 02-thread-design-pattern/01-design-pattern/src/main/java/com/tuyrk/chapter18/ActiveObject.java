2b2ca47cd3a70239737f8dab29b5a9d7d3c593dc
zhaoxiangrui <15546113287@163.com>
2020-11-02 00:12:41 +0800

    chapter18

---
+++ 02-thread-design-pattern/01-design-pattern/src/main/java/com/tuyrk/chapter18/ActiveObject.java
@@ -0,0 +1,8 @@
+package com.tuyrk.chapter18;
+
+/**
+ * @author zhaoxiangrui
+ * @date 2020/11/1 23:32
+ */
+public class ActiveObject {
+}
---
+++ 02-thread-design-pattern/01-design-pattern/src/main/java/com/tuyrk/chapter18/MessageRequest.java
@@ -0,0 +1,8 @@
+package com.tuyrk.chapter18;
+
+/**
+ * @author zhaoxiangrui
+ * @date 2020/11/1 23:41
+ */
+public class MessageRequest {
+}
